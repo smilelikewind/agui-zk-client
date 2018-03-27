88dec437be4e79de3d04a9232204a58e398b81b0
xiaowei <xiaowei.li@owitho.com>
2018-03-27 11:05:54 +0800

    master

--- src/main/java/com/agui/zk/client/constants/ZKConstants.java
+++ src/main/java/com/agui/zk/client/constants/ZKConstants.java
@@ -6,7 +6,13 @@ package com.agui.zk.client.constants;
  * @author xiaowei.li
  */
 public class ZKConstants {
+    /**
+     * zk 连接的Ip和端口
+     */
     public static String zkServerAddress = "127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183";
+    /**
+     * 每次sessionTimeout时间
+     */
     public static int sessionTimeOut = 2000;
     public static int zkMonitorExecuteIntervalTime = 1000;
     public static String basePath = "/data/agui/";
